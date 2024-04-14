bb491fd500eee0217bb7d3a7c1aefccfbb429b11
ShinyMorgana <89910002+ShinyMorgana@users.noreply.github.com>
2024-04-14 21:01:38 +0200

    zzz

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\BoardGameController;
 use App\Http\Controllers\API\RegisterController;
 
-// Place the Board Games route first
+
 Route::get('api/boardgames', [BoardGameController::class, 'index']);
 Route::post('api/boardgames', [BoardGameController::class, 'store']);
 Route::post('/boardgames', [BoardGameController::class, 'store']);
-
-
-
-// Place the Register route
-Route::post('/register', [RegisterController::class, 'register']);
-
-// Any additional routes can go here
