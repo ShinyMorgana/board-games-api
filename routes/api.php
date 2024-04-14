bf6e2c9a65b615afc05c63501837b08b90955602
ShinyMorgana <89910002+ShinyMorgana@users.noreply.github.com>
2024-04-14 19:46:56 +0200

    sss

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\API\RegisterController;
 // Place the Board Games route first
 Route::get('api/boardgames', [BoardGameController::class, 'index']);
 Route::post('api/boardgames', [BoardGameController::class, 'store']);
+Route::post('/boardgames', [BoardGameController::class, 'store']);
 
 
 
